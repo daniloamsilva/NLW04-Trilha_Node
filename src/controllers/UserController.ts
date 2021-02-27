3506e2f0ee213cc136b4b3c67bf4329cbf9e9e66
Danilo Augusto <danilo.augmonteiro@gmail.com>
2021-02-27 19:00:11 -0300

    Aula 05

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 
 import { UsersRepository } from '../repositories/UsersRepository';
 
@@ -7,7 +9,18 @@ class UserController {
 
   async create(request: Request, response: Response){
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
     
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      throw new AppError(err);
+    }
+
     const userRepository = getCustomRepository(UsersRepository);
 
     const userAlreadyExists = await userRepository.findOne({
@@ -15,9 +28,7 @@ class UserController {
     });
 
     if(userAlreadyExists){
-      return response.status(400).json({
-        error: "User already exists!",
-      });
+      throw new AppError("User already exists!");
     }
 
     const user = userRepository.create({
